edcf4b6bcdd4159548e833651fc0d5de42e457a5
yamulla <ilnurguraba1441@gmail.com>
2026-07-25 22:26:33 +0500

    Homework 6

---
+++ main.js
@@ -0,0 +1,55 @@
+const mainTitle = '.product-card__image-box'; // camelCase
+const MAINTITLE = '.product-card__image-box'; // upperCase
+const main_title = '.product-card__image-box'; // snake_case
+
+// Покраска всех карточек
+
+const productCards = document.querySelectorAll('.product-card__image-box');
+const changeColorAllCardButton = document.querySelector('#change-color-all-card');
+const greenColorHash = '#00FF00';
+const blueColorHash = '#000077';
+
+changeColorAllCardButton.addEventListener('click', () => {
+  productCards.forEach((card) => card.style.backgroundColor = greenColorHash)
+})
+
+// Покраска первой карточки
+
+const firstProductCard = document.querySelector('.product-card__image-box');
+const changeColorFirstCardButton = document.querySelector('#change-color-first-card');
+
+changeColorFirstCardButton.addEventListener('click', () => {
+  firstProductCard.style.backgroundColor = blueColorHash;
+})
+
+// Открыть google
+
+const openGoogleButton = document.querySelector('#open-google');
+
+openGoogleButton.addEventListener('click', openGoogle)
+
+function openGoogle() {
+  const answer = confirm('Вы действительно хотите открыть Google?');
+
+  if (answer === true) {
+    window.open('https://google.com')
+  } else {
+    return;
+  }
+}
+
+// Вывод консоль лог
+
+const outputLogButton = document.querySelector('#output-console-log');
+
+outputLogButton.addEventListener('click', () => outputConsoleLog('ДЗ №4'))
+
+function outputConsoleLog(message) {
+  alert(message)
+  console.log(message)
+}
+
+document.getElementById('show-time').addEventListener('click', () => {
+    const currentTime = new Date().toLocaleTimeString();
+    alert(`Текущее время: ${currentTime}`);
+});
